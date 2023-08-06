81930ee382b8659482cb0bc02ccba13ad975bdb6
aditya-jha-1999 <adityajhaengineer1999@gmail.com>
2023-08-06 08:29:17 +0530

    Array and object

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//Introduction ###########
+//conditional statement
+//Array & object
+//loop 
+//functions
+//string number boolean null bigint
+//object arrays
+
+var obj = {
+    name: "aditya babu",
+    id: 32,
+    test: ["fail", "pass", "fail", "pass"],
+    exam: {
+        minterm: "fail",
+        final: "pass"
+
+    },
+    isgoodstructure: true
+};
+console.log(obj);
